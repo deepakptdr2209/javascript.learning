2409b1fdd8d0aee0a5c5f0a874f68e1d890a8838
deepakptdr2209 <123632115+deepakptdr2209@users.noreply.github.com>
2024-03-13 13:35:23 +0000

    function done

---
+++ .devcontainer/tut3/functions.js
@@ -0,0 +1,77 @@
+
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+
+
+//+++++++++++ Rest operator(...) +++++++++++++
+// it is uesd if we give more thsn one argument on calling fun
+// this will put aall the argument in array 
+function calculateCartPrice(val1, val2, ...prices){  
+    return prices
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))   // array of all prices
+
+//++++++++++++++ object fun +++++++++
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+//+++++++++++++ fun with array +++++++++++++++++
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
